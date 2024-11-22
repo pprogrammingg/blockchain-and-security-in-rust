350fe5afb7c8aa16f1e1ee741882dd2975fb0c14
Pprog <29218920+pprogrammingg@users.noreply.github.com>
2024-11-22 12:18:50 -0500

    feat: proof of existence

--- web3dev_blockchain_from_scratch/src/lib.rs
+++ web3dev_blockchain_from_scratch/src/lib.rs
@@ -2,3 +2,5 @@ pub mod balances;
 pub mod system;
 
 pub mod support;
+
+pub mod proof_of_existence;
---
+++ web3dev_blockchain_from_scratch/src/proof_of_existence.rs
@@ -0,0 +1,93 @@
+use core::fmt::Debug;
+use std::collections::BTreeMap;
+
+use crate::support::DispatchResult;
+
+pub trait Config: crate::system::Config {
+    type Content: Debug + Ord;
+}
+
+/// This is the Proof of Existence Module.
+/// It is a simple module that allows accounts to claim existence of some data.
+#[derive(Debug)]
+pub struct Pallet<T: Config> {
+    /// A simple storage map from content to the owner of that content.
+    /// Accounts can make multiple different claims, but each claim can only have one owner.
+    claims: BTreeMap<T::Content, T::AccountId>,
+}
+
+impl<T: Config> Pallet<T> {
+    /// Create a new instance of the Proof of Existence Module.
+    pub fn new() -> Self {
+        Pallet {
+            claims: BTreeMap::new(),
+        }
+    }
+
+    pub fn get_claim(&self, claim: &T::Content) -> Option<&T::AccountId> {
+        self.claims.get(claim)
+    }
+
+    // add claim
+    pub fn create_claim(&mut self, caller: T::AccountId, claim: T::Content) -> DispatchResult {
+        match self.get_claim(&claim) {
+            Some(_) => Err("Claim already exists"),
+            None => {
+                self.claims
+                    .insert(claim, caller);
+                Ok(())
+            }
+        }
+    }
+
+    // revoke claim
+    pub fn revoke_claim(&mut self, caller: T::AccountId, claim: T::Content) -> DispatchResult {
+        let claim_owner = self
+            .get_claim(&claim)
+            .ok_or("Claim does not exist")?;
+
+        if claim_owner != &caller {
+            return Err("Caller is not the owner of the claim");
+        }
+
+        self.claims.remove(&claim);
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::system::Config;
+
+    struct TestConfig;
+
+    impl Config for TestConfig {
+        type AccountId = &'static str;
+        type BlockNumber = u32;
+        type Nonce = u32;
+    }
+
+    impl super::Config for TestConfig {
+        type Content = &'static str;
+    }
+
+    #[test]
+    fn basic_proof_of_existence() {
+        let mut poe = super::Pallet::<TestConfig>::new();
+        let _ = poe.create_claim("alice", "my_document");
+        assert_eq!(poe.get_claim(&"my_document"), Some(&"alice"));
+
+        let res = poe.revoke_claim(&"bob", "my_document");
+        assert_eq!(res, Err("Caller is not the owner of the claim"));
+
+        let res = poe.create_claim("bob", "my_document");
+        assert_eq!(res, Err("Claim already exists"));
+
+        let res = poe.revoke_claim("alice", "non existent");
+        assert_eq!(res, Err("Claim does not exist"));
+
+        let _ = poe.revoke_claim("alice", "my_document");
+        assert_eq!(poe.get_claim(&"my_document"), None);
+    }
+}
